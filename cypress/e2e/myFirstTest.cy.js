b8305b7fcb6bb27e0bb354b329fb05e2ba29aee6
Nikhil Gupta <nikhil.gupta@example.com>
2026-04-24 13:21:57 +0530

    Initial commit with Cypress and CI/CD setup

---
+++ cypress/e2e/myFirstTest.cy.js
@@ -0,0 +1,14 @@
+describe('My First Test', () => {
+  it('test1', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/')
+    // cy.contains('type').click()
+    // cy.url().should('include', '/commands/actions')
+    // cy.get('.action-email').type('hello@cypress.io')
+    cy.title().should('eq', 'OrangeHRM') 
+  })
+
+  it('negative testing', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/')
+    cy.title().should('eq', 'OrangeHRM1234')
+  })
+})
